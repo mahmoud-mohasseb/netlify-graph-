873a927450a839c9017da3434f79638febd31278
Mac X <ghareb4@gmail.com>
2022-11-18 23:22:22 +0200

    express-graphql

--- express/server.js
+++ express/server.js
@@ -1,22 +1,45 @@
-'use strict';
-const express = require('express');
-const path = require('path');
-const serverless = require('serverless-http');
+"use strict";
+const express = require("express");
+const path = require("path");
+const serverless = require("serverless-http");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
+
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello world Express graphql on netlify mahmoud !";
+  },
+};
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+router.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Hello from Express.js!</h1>");
   res.end();
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get("/another", (req, res) => res.json({ route: req.originalUrl }));
+router.post("/", (req, res) => res.json({ postBody: req.body }));
 
+router.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use("/.netlify/functions/server", router); // path must route to lambda
+app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
